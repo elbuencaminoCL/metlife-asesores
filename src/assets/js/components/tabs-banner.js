88455cbfe9f9c5171a6f0e5575a63f1dd70a6b15
svhen <sgarcia@svhen.com>
2017-11-14 15:23:10 -0500

    Inputs home

--- src/assets/js/components/tabs-banner.js
+++ src/assets/js/components/tabs-banner.js
@@ -1,4 +1,22 @@
 
 import $ from 'jquery'
 
-let $openContent = $( '.openContent' );
+$( '.openContentInputRut' ).on( 'click', ( e ) => {
+  e.preventDefault();
+
+  $('.contentInputRut').removeClass('hide-xs')
+  $('.buttonRut').addClass('hide-xs')
+  $( '.openContentInputRut' ).removeClass('col-xs-9')
+  $( '.openContentInputRut' ).addClass('col-xs-12')
+
+});
+
+$( '.openContentInputPolicy' ).on( 'click', ( e ) => {
+  e.preventDefault();
+
+  $('.contentInputPolicy').removeClass('hide-xs')
+  $('.buttonPolicy').addClass('hide-xs')
+  $( '.openContentInputPolicy' ).removeClass('col-xs-9')
+  $( '.openContentInputPolicy' ).addClass('col-xs-12')
+
+});
