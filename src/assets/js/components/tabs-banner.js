255401255f829d3104b2afde208cfbafe0f30ae2
Jair Garcia <jgarcia@svhen.com>
2017-11-14 20:16:19 -0500

    Banner home styles fix

--- src/assets/js/components/tabs-banner.js
+++ src/assets/js/components/tabs-banner.js
@@ -5,20 +5,21 @@ let $dropdownRow
 
 $( '.heightInput' ).on( 'click', ( e ) => {
   e.preventDefault();
-  console.log('sdfshbdf')
 
   if( $(e.currentTarget).hasClass('dropdownOpen') ) {
-    const $bannerContentColTabs = parseInt($('.banner-content-col-tabs').css('bottom'))
+    const $bannerContentColTabs = parseInt($(e.currentTarget).closest('.banner-content-col-tabs').css('bottom'))
     const $result = $bannerContentColTabs + $dropdownRow
-    $('.banner-content-col-tabs').css('bottom', $result + 'px')
+
+    $(e.currentTarget).closest('.banner-content-col-tabs').css('bottom', $result + 'px')
     $(e.currentTarget).removeClass('dropdownOpen')
   }
   else {
-    $dropdownRow = $('.dropdownRow').height()
-    const $bannerContentColTabs = parseInt($('.banner-content-col-tabs').css('bottom'))
-    const $result = $bannerContentColTabs - $dropdownRow
-    $('.banner-content-col-tabs').css('bottom', $result + 'px')
+    $dropdownRow = $(e.currentTarget).closest('.tabs-panel').find('.dropdownRow').height()
 
+    const $bannerContentColTabs = parseInt($(e.currentTarget).closest('.banner-content-col-tabs').css('bottom'))
+    const $result = $bannerContentColTabs - $dropdownRow
+
+    $(e.currentTarget).closest('.banner-content-col-tabs').css('bottom', $result + 'px')
     $(e.currentTarget).addClass('dropdownOpen')
   }
 });
