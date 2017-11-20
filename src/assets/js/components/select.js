492badd94d6a7f7e42748ee69958913a925fd945
Jair Garcia <jgarcia@svhen.com>
2017-11-20 14:52:25 -0500

    Select checkbox

--- src/assets/js/components/select.js
+++ src/assets/js/components/select.js
@@ -3,39 +3,57 @@
 	import $ from 'jquery';
 
   [].slice.call( document.querySelectorAll( 'select.GeneralSelect.cs-select' ) ).forEach( (el) => {
-    new SelectFx(el);
+    new SelectFx(el)
   });
 
   $(".dropdown dt a").on('click', function() {
-    $(".dropdown dd ul").slideToggle('fast');
+    $(".dropdown dd ul").slideToggle('fast')
   });
 
   $(".dropdown dd ul li a").on('click', function() {
-    $(".dropdown dd ul").hide();
+    $(".dropdown dd ul").hide()
   });
 
   function getSelectedValue(id) {
-    return $("#" + id).find("dt a span.value").html();
+    return $("#" + id).find("dt a span.value").html()
   }
 
   $(document).bind('click', function(e) {
-    var $clicked = $(e.target);
-    if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
+    let $clicked = $(e.target)
+    if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide()
   });
 
-  $('.mutliSelect input[type="checkbox"]').on('click', function() {
+  let arraySelectCheckbox = []
 
-    var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
-      title = $(this).val() + ",";
+  $('.mutliSelect input[type="checkbox"]').on('click', function(e) {
 
-    if ($(this).is(':checked')) {
-      var html = '<span title="' + title + '">' + title + '</span>';
-      $('.multiSel').append(html);
-      $(".hida").hide();
-    } else {
-      $('span[title="' + title + '"]').remove();
-      var ret = $(".hida");
-      $('.dropdown dt a').append(ret);
+    let title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val()
+        title = $(this).val() + ","
 
+    if( $(this).is(':checked') ) {
+      let html = `<span title="${title}">${title}</span>`
+      $('.multiSel').append(html)
+      $('.hida').hide()
     }
+    else {
+      $('span[title="' + title + '"]').remove()
+      let ret = $(".hida")
+      $('.dropdown dt a').append(ret)
+    }
+
+    $('.mutliSelect li').each( (index, el) => {
+      const checkbox = $(el).find('input[type="checkbox"]')
+
+      if( $(checkbox).is(':checked') ) {
+        console.log(checkbox, 'checkbox')
+      }
+    })
+
+    if( $('.multiSel').text().length == 0 ) {
+      $(".hida").show()
+    }
+    else {
+      $(".hida").hide()
+    }
+
   });
